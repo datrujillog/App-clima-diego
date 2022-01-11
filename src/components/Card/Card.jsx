b92741ddbbca5bb4d351fb05224d017f2d15c835
yego0210 <diegotrujillo0210@gmail.com>
2022-01-11 16:02:10 -0500

    App-Clima

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React from 'react'
 import s from './Card.module.css'
-export default function Card(props) {
-  const {max, min, name, img, onClose} = props
+
+export default function Card (props) {
+  const { max, min, name, img, onClose } = props
   // acá va tu código
   return (
-  <div className={s.cardContainer}>
-    <button className={s.button} onClick={onClose}>X</button>
-    <h3 className={s.title}>{name}</h3>
-    <div className={s.bodyContainer}>
-      <div className={s.temperatureContainer}>
-        <div className={s.temperature}>
-          <p className={s.subTitle}>min</p>
-          <p>{min}</p>
+    <div className={s.cardContainer}>
+      <button className={s.button} onClick={onClose}>
+        X
+      </button>
+      <h3 className={s.title}>{name}</h3>
+      <div className={s.bodyContainer}>
+        <div className={s.temperatureContainer}>
+          <div className={s.temperature}>
+            <p className={s.subTitle}>min</p>
+            <p>{min}</p>
+          </div>
+          <div className={s.temperature}>
+            <p className={s.subTitle}>max</p>
+            <p>{max}</p>
+          </div>
         </div>
-        <div className={s.temperature}>
-          <p className={s.subTitle}>max</p>
-          <p>{max}</p>
-        </div>
+        <figure>
+          <img
+            className={s.imgCard}
+            src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+            alt='solcito'
+          />
+        </figure>
+        <br />
       </div>
-      <figure>
-        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="solcito" />
-      </figure>
     </div>
-  </div>
   )
-};
+}
